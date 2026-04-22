c2ce5c60affb00b39bdecc36d151bf6d1586bee3
hedron-network <146460512+hedron-network@users.noreply.github.com>
2026-04-22 17:59:09 +0200

    chaged project structure to python dependency

--- code/main.cpp
+++ code/main.cpp
@@ -4,13 +4,5 @@
 
 int main()
 {
-    std::cout<<"test"<<std::endl;
-    unsigned short board = 0;
-    BoardHandling::MakeUncheckedMove(BoardHandling::IntToBoard(1),board);
-    BoardHandling::MakeUncheckedMove(BoardHandling::IntToBoard(5),board);
-    auto moves = BoardHandling::AviableMoves(board,0);
-    for(int move:moves){
-        std::cout<<move<<" |";
-    }
     return 0;
 }
